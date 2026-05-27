182c2e24ad40ea9b56321ebd2475f9139ae640c1
Андрей <andrey.buharov@gmail.com>
2026-05-28 00:25:42 +0300

    Автокоммит изменений в people

--- init.php
+++ init.php
@@ -1,3 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('PEOPLE_VERSION') OR define('PEOPLE_VERSION', '1.0.1');
 
+Kohana::$config->load('menu')
+    ->set('people', array(
+        'title' => 'Сотрудники',
+        'url' => 'people/peopleinfo',
+        'icon' => 'fa-cog',
+        'order' => 30,
+       
+    ));
